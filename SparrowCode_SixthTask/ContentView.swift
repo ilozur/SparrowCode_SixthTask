451a707b89f7b274859c842581508b5089d44877
Edmond <edpodlegaev@yandex.ru>
2023-12-16 13:21:42 +0300

    Fix bugs & Refactor & Complete task

--- SparrowCode_SixthTask/ContentView.swift
+++ SparrowCode_SixthTask/ContentView.swift
@@ -8,30 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    let countOfRectangles: Int = 12
+    let countOfRectangles: Int = 7
     @State private var isPressed: Bool = false
     
     var body: some View {
         GeometryReader { geometry in
-            let layout = isPressed ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+            let layout = isPressed ? AnyLayout(VStackLayout(spacing: 0)) : AnyLayout(HStackLayout())
             layout {
-                ForEach(0 ..< countOfRectangles, id: \.self) { rectangle in
+                ForEach(isPressed ? Array(0 ... countOfRectangles - 1).reversed() : Array(0 ... countOfRectangles - 1), id: \.self) { rectangle in
                     RoundedRectangle(cornerRadius: geometry.size.height / CGFloat(countOfRectangles) / 10)
-                    .foregroundStyle(Color.blue)
-                    .aspectRatio(contentMode: .fit)
-                    .offset(
-                        x: isPressed ? CGFloat(rectangle) * (geometry.size.width - geometry.size.height / CGFloat(countOfRectangles)) / CGFloat(countOfRectangles - 1) : 0,
-                        y: isPressed ? 0 : geometry.size.height / 2)
-                    .onTapGesture {
-                        withAnimation {
-                            isPressed.toggle()
+                        .foregroundStyle(Color.blue)
+                        .aspectRatio(contentMode: .fit)
+                        .offset(
+                            x: isPressed ? CGFloat(rectangle) * (geometry.size.width - geometry.size.height / CGFloat(countOfRectangles)) / CGFloat(countOfRectangles - 1) : 0,
+                            y: isPressed ? 0 : geometry.size.height / 2)
+                        .onTapGesture {
+                            withAnimation {
+                                isPressed.toggle()
+                            }
                         }
-                    }
                 }
             }
         }
     }
 }
+
 #Preview {
     ContentView()
 }
