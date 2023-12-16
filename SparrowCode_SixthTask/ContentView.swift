393ed27bc78a506fa8301c95c41757eb7e696c8d
Edmond <edpodlegaev@yandex.ru>
2023-12-16 13:00:34 +0300

    Add main animation

--- SparrowCode_SixthTask/ContentView.swift
+++ SparrowCode_SixthTask/ContentView.swift
@@ -7,18 +7,21 @@
 
 import SwiftUI
 
-let countOfRectangles: Int = 7
-var isPressed: Bool = false
-
 struct ContentView: View {
+    let countOfRectangles: Int = 12
+    @State private var isPressed: Bool = false
+    
     var body: some View {
         GeometryReader { geometry in
             let layout = isPressed ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
             layout {
-                ForEach(0 ..< countOfRectangles, id: \.self) { _ in
-                    RoundedRectangle(cornerRadius: 10)
+                ForEach(0 ..< countOfRectangles, id: \.self) { rectangle in
+                    RoundedRectangle(cornerRadius: geometry.size.height / CGFloat(countOfRectangles) / 10)
                     .foregroundStyle(Color.blue)
                     .aspectRatio(contentMode: .fit)
+                    .offset(
+                        x: isPressed ? CGFloat(rectangle) * (geometry.size.width - geometry.size.height / CGFloat(countOfRectangles)) / CGFloat(countOfRectangles - 1) : 0,
+                        y: isPressed ? 0 : geometry.size.height / 2)
                     .onTapGesture {
                         withAnimation {
                             isPressed.toggle()
